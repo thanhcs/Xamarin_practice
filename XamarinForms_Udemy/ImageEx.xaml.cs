7a91535ca42a9df0b370650c7b0b1e9288d41c8d
Thanh Nguyen <thanhnguyencs@utexas.edu>
2017-04-05 07:59:59 -0500

    Finished Image Ex

--- XamarinForms_Udemy/ImageEx.xaml.cs
+++ XamarinForms_Udemy/ImageEx.xaml.cs
@@ -12,13 +12,45 @@ namespace XamarinForms_Udemy
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ImageEx : ContentPage
     {
+        int count;
         public ImageEx()
         {
             InitializeComponent();
-            image.Source = new UriImageSource {
-                Uri = new Uri("http://lorempixel.com/1920/1080/city/1"),
+            count = 1;
+            image.Source = getImage();
+        }
+
+        private void OnNextHandler(object sender, EventArgs e)
+        {
+            if (count == 10)
+            {
+                count = 1;
+            } else
+            {
+                ++count;
+            }
+            image.Source = getImage();
+        }
+
+        private void OnPre(object sender, EventArgs e)
+        {
+            if (count == 1)
+            {
+                count = 10;
+            } else
+            {
+                --count;
+            }
+            image.Source = getImage();
+        }
+
+        private UriImageSource getImage()
+        {
+            return new UriImageSource
+            {
+                Uri = new Uri($"http://lorempixel.com/1920/1080/city/{count}"),
                 CachingEnabled = false,
-                CacheValidity = TimeSpan.FromHours(1);
+                CacheValidity = TimeSpan.FromHours(1)
             };
         }
     }
